02daa54084798191086a0eab3d9a958eebc962af
taoufik321 <taoufik321@users.noreply.github.com>
2018-11-25 15:09:41 +0100

    Basis opgezet voor de tweede opdracht.

---
+++ src/main/java/opdracht2/Product.java
@@ -0,0 +1,23 @@
+package opdracht2;
+
+public class Product implements Supermarkt {
+    public void toevoegen() {
+        System.out.println("test");
+    }
+
+    public void verwijderen() {
+
+    }
+
+    public void afrekenen() {
+
+    }
+
+    public void wisselgeld() {
+
+    }
+
+    public void totaleKorting() {
+
+    }
+}
---
+++ src/main/java/opdracht2/Supermarkt.java
@@ -0,0 +1,13 @@
+package opdracht2;
+
+// Hier zitten alle methodes waar een product rekeneing mee moet houden
+public interface Supermarkt {
+    void toevoegen();
+    void verwijderen();
+
+    void afrekenen();
+
+    void wisselgeld();
+
+    void totaleKorting();
+}
